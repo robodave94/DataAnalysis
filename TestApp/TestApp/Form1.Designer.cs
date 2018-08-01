561a21f19323fb28f38d035167d9f61c1149af3a
DavidH <David.Hinwood@canberra.edu.au>
2018-08-01 17:27:44 +1000

    First Data

---
+++ TestApp/TestApp/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace TestApp
+{
+    partial class colToUse
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chseDirectory = new System.Windows.Forms.Button();
+            this.flType = new System.Windows.Forms.TextBox();
+            this.dirLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.FileFnd = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.findFiles = new System.Windows.Forms.Button();
+            this.genCSVclc = new System.Windows.Forms.Button();
+            this.LstOfNames = new System.Windows.Forms.ListBox();
+            this.lstofColoumns = new System.Windows.Forms.ListBox();
+            this.cngName = new System.Windows.Forms.Button();
+            this.addColomn = new System.Windows.Forms.Button();
+            this.nametxtbx = new System.Windows.Forms.TextBox();
+            this.coltxtbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rmvCol = new System.Windows.Forms.Button();
+            this.rmFileRef = new System.Windows.Forms.Button();
+            this.cmboBxSepVal = new System.Windows.Forms.ComboBox();
+            this.slctSepVal = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.OutptFileTxt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // chseDirectory
+            // 
+            this.chseDirectory.Location = new System.Drawing.Point(12, 12);
+            this.chseDirectory.Name = "chseDirectory";
+            this.chseDirectory.Size = new System.Drawing.Size(155, 23);
+            this.chseDirectory.TabIndex = 0;
+            this.chseDirectory.Text = "Choose Directory";
+            this.chseDirectory.UseVisualStyleBackColor = true;
+            this.chseDirectory.Click += new System.EventHandler(this.chseDirectory_Click);
+            // 
+            // flType
+            // 
+            this.flType.Location = new System.Drawing.Point(68, 43);
+            this.flType.Name = "flType";
+            this.flType.Size = new System.Drawing.Size(99, 20);
+            this.flType.TabIndex = 1;
+            // 
+            // dirLabel
+            // 
+            this.dirLabel.AutoSize = true;
+            this.dirLabel.Location = new System.Drawing.Point(173, 17);
+            this.dirLabel.Name = "dirLabel";
+            this.dirLabel.Size = new System.Drawing.Size(111, 13);
+            this.dirLabel.TabIndex = 2;
+            this.dirLabel.Text = "No Directory Selected";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "File Type";
+            // 
+            // FileFnd
+            // 
+            this.FileFnd.FormattingEnabled = true;
+            this.FileFnd.Location = new System.Drawing.Point(13, 133);
+            this.FileFnd.Name = "FileFnd";
+            this.FileFnd.Size = new System.Drawing.Size(709, 342);
+            this.FileFnd.TabIndex = 4;
+            this.FileFnd.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Files Found";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // findFiles
+            // 
+            this.findFiles.Location = new System.Drawing.Point(18, 69);
+            this.findFiles.Name = "findFiles";
+            this.findFiles.Size = new System.Drawing.Size(149, 36);
+            this.findFiles.TabIndex = 6;
+            this.findFiles.Text = "Find Files";
+            this.findFiles.UseVisualStyleBackColor = true;
+            this.findFiles.Click += new System.EventHandler(this.fndFiles_Click);
+            // 
+            // genCSVclc
+            // 
+            this.genCSVclc.Location = new System.Drawing.Point(728, 7);
+            this.genCSVclc.Name = "genCSVclc";
+            this.genCSVclc.Size = new System.Drawing.Size(150, 39);
+            this.genCSVclc.TabIndex = 7;
+            this.genCSVclc.Text = "Generate CSV";
+            this.genCSVclc.UseVisualStyleBackColor = true;
+            this.genCSVclc.Click += new System.EventHandler(this.gencsv_Click);
+            // 
+            // LstOfNames
+            // 
+            this.LstOfNames.FormattingEnabled = true;
+            this.LstOfNames.Location = new System.Drawing.Point(728, 133);
+            this.LstOfNames.Name = "LstOfNames";
+            this.LstOfNames.Size = new System.Drawing.Size(150, 342);
+            this.LstOfNames.TabIndex = 8;
+            // 
+            // lstofColoumns
+            // 
+            this.lstofColoumns.FormattingEnabled = true;
+            this.lstofColoumns.Location = new System.Drawing.Point(884, 172);
+            this.lstofColoumns.Name = "lstofColoumns";
+            this.lstofColoumns.Size = new System.Drawing.Size(150, 303);
+            this.lstofColoumns.TabIndex = 9;
+            // 
+            // cngName
+            // 
+            this.cngName.Location = new System.Drawing.Point(728, 105);
+            this.cngName.Name = "cngName";
+            this.cngName.Size = new System.Drawing.Size(150, 23);
+            this.cngName.TabIndex = 10;
+            this.cngName.Text = "Click to change Name";
+            this.cngName.UseVisualStyleBackColor = true;
+            this.cngName.Click += new System.EventHandler(this.cngName_Click);
+            // 
+            // addColomn
+            // 
+            this.addColomn.Location = new System.Drawing.Point(884, 104);
+            this.addColomn.Name = "addColomn";
+            this.addColomn.Size = new System.Drawing.Size(73, 62);
+            this.addColomn.TabIndex = 11;
+            this.addColomn.Text = "Click to add column";
+            this.addColomn.UseVisualStyleBackColor = true;
+            this.addColomn.Click += new System.EventHandler(this.addColomn_Click);
+            // 
+            // nametxtbx
+            // 
+            this.nametxtbx.Location = new System.Drawing.Point(728, 79);
+            this.nametxtbx.Name = "nametxtbx";
+            this.nametxtbx.Size = new System.Drawing.Size(150, 20);
+            this.nametxtbx.TabIndex = 12;
+            // 
+            // coltxtbox
+            // 
+            this.coltxtbox.Location = new System.Drawing.Point(884, 78);
+            this.coltxtbox.Name = "coltxtbox";
+            this.coltxtbox.Size = new System.Drawing.Size(150, 20);
+            this.coltxtbox.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(728, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Names for files";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(881, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Columns to Read";
+            // 
+            // rmvCol
+            // 
+            this.rmvCol.Location = new System.Drawing.Point(961, 104);
+            this.rmvCol.Name = "rmvCol";
+            this.rmvCol.Size = new System.Drawing.Size(73, 62);
+            this.rmvCol.TabIndex = 16;
+            this.rmvCol.Text = "Click to remove column";
+            this.rmvCol.UseVisualStyleBackColor = true;
+            this.rmvCol.Click += new System.EventHandler(this.rmvCol_Click);
+            // 
+            // rmFileRef
+            // 
+            this.rmFileRef.Location = new System.Drawing.Point(532, 105);
+            this.rmFileRef.Name = "rmFileRef";
+            this.rmFileRef.Size = new System.Drawing.Size(189, 23);
+            this.rmFileRef.TabIndex = 17;
+            this.rmFileRef.Text = "Remove File From Concationation";
+            this.rmFileRef.UseVisualStyleBackColor = true;
+            this.rmFileRef.Click += new System.EventHandler(this.rmFileRef_Click);
+            // 
+            // cmboBxSepVal
+            // 
+            this.cmboBxSepVal.FormattingEnabled = true;
+            this.cmboBxSepVal.Items.AddRange(new object[] {
+            "  -  Tabs",
+            ", -  Comma",
+            ". -  Period"});
+            this.cmboBxSepVal.Location = new System.Drawing.Point(348, 54);
+            this.cmboBxSepVal.Name = "cmboBxSepVal";
+            this.cmboBxSepVal.Size = new System.Drawing.Size(126, 21);
+            this.cmboBxSepVal.TabIndex = 18;
+            // 
+            // slctSepVal
+            // 
+            this.slctSepVal.AutoSize = true;
+            this.slctSepVal.Location = new System.Drawing.Point(348, 36);
+            this.slctSepVal.Name = "slctSepVal";
+            this.slctSepVal.Size = new System.Drawing.Size(126, 13);
+            this.slctSepVal.TabIndex = 19;
+            this.slctSepVal.Text = "Select Separation Values";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(885, 11);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Generated File Name";
+            // 
+            // OutptFileTxt
+            // 
+            this.OutptFileTxt.Location = new System.Drawing.Point(884, 27);
+            this.OutptFileTxt.Name = "OutptFileTxt";
+            this.OutptFileTxt.Size = new System.Drawing.Size(150, 20);
+            this.OutptFileTxt.TabIndex = 21;
+            // 
+            // colToUse
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1053, 485);
+            this.Controls.Add(this.OutptFileTxt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.slctSepVal);
+            this.Controls.Add(this.cmboBxSepVal);
+            this.Controls.Add(this.rmFileRef);
+            this.Controls.Add(this.rmvCol);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.coltxtbox);
+            this.Controls.Add(this.nametxtbx);
+            this.Controls.Add(this.addColomn);
+            this.Controls.Add(this.cngName);
+            this.Controls.Add(this.lstofColoumns);
+            this.Controls.Add(this.LstOfNames);
+            this.Controls.Add(this.genCSVclc);
+            this.Controls.Add(this.findFiles);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.FileFnd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dirLabel);
+            this.Controls.Add(this.flType);
+            this.Controls.Add(this.chseDirectory);
+            this.Name = "colToUse";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button chseDirectory;
+        private System.Windows.Forms.TextBox flType;
+        private System.Windows.Forms.Label dirLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox FileFnd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button findFiles;
+        private System.Windows.Forms.Button genCSVclc;
+        private System.Windows.Forms.ListBox LstOfNames;
+        private System.Windows.Forms.ListBox lstofColoumns;
+        private System.Windows.Forms.Button cngName;
+        private System.Windows.Forms.Button addColomn;
+        private System.Windows.Forms.TextBox nametxtbx;
+        private System.Windows.Forms.TextBox coltxtbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button rmvCol;
+        private System.Windows.Forms.Button rmFileRef;
+        private System.Windows.Forms.ComboBox cmboBxSepVal;
+        private System.Windows.Forms.Label slctSepVal;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox OutptFileTxt;
+    }
+}
+
---
+++ TestApp/TestApp/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace TestApp
+{
+    public partial class colToUse : Form
+    {
+        List<List<string>> writableContent = new List<List<string>>();
+        public colToUse()
+        {
+            InitializeComponent();
+        }
+
+        private void chseDirectory_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+            folderDialog.ShowDialog();
+            dirLabel.Text = folderDialog.SelectedPath;
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fndFiles_Click(object sender, EventArgs e)
+        {
+            FindData(dirLabel.Text);
+            //string[] files = Directory.GetFiles(dirLabel.Text);
+            //System.Windows.Forms.MessageBox.Show("Files found: " + files[0], "Message");
+            //Recursivly find all files ending in .file extension
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        #region RecursiveAnalysis
+        public void FindData(string Path)
+        {   var args = Directory.EnumerateFileSystemEntries(dirLabel.Text);
+            foreach (string path in args)
+            {
+                if (File.Exists(path))
+                {
+                    // This path is a file
+                    ProcessFile(path);
+                }
+                else if (Directory.Exists(path))
+                {
+                    // This path is a directory
+                    ProcessDirectory(path);
+                }
+            }
+        }
+        
+        public void ProcessDirectory(string targetDirectory)
+        {
+            // Process the list of files found in the directory.
+            string[] fileEntries = Directory.GetFiles(targetDirectory);
+            foreach (string fileName in fileEntries)
+                ProcessFile(fileName);
+
+            // Recurse into subdirectories of this directory.
+            string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+            foreach (string subdirectory in subdirectoryEntries)
+                ProcessDirectory(subdirectory);
+        }
+        public void ProcessFile(string path)
+        {
+            if (path.Substring(path.Length - (flType.Text.Length + 1)) == "." + flType.Text)
+            {
+                FileFnd.Items.Add(path);
+                LstOfNames.Items.Add(path.Substring(path.Length - 10));
+            }
+        }
+        #endregion
+
+        private void cngName_Click(object sender, EventArgs e)
+        {
+            LstOfNames.Items[LstOfNames.SelectedIndex] = nametxtbx.Text;
+        }
+
+        private void addColomn_Click(object sender, EventArgs e)
+        {
+            lstofColoumns.Items.Add(coltxtbox.Text);
+        }
+        
+        private void rmvCol_Click(object sender, EventArgs e)
+        {
+            lstofColoumns.Items.RemoveAt(lstofColoumns.SelectedIndex);
+        }
+
+        private void gencsv_Click(object sender, EventArgs e)
+        {
+            string sep;
+            switch (cmboBxSepVal.SelectedItem.ToString())
+            {
+                case "  -  Tabs":
+                    sep = "\t";
+                    break;
+                case ", -Comma":
+                    sep = ",";
+                    break;
+                case ". - Period":
+                    sep = ".";
+                    break;
+                default:
+                    throw new System.ArgumentException("Parameter cannot be null", "original");
+            }
+            //foreach (string path in FileFnd.Items) {
+            for (int o = 0; o < FileFnd.Items.Count; o++)
+            {
+                readFile(FileFnd.Items[o].ToString(), sep, LstOfNames.Items[o].ToString());
+            }
+            int max_count = 0;
+            foreach (List<string> ls in writableContent)
+            {
+                if (ls.Count > max_count)
+                {
+                    max_count = ls.Count;
+                }
+            }
+            string content = "";
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(dirLabel.Text+"\\"+ OutptFileTxt.Text+".csv"))
+            {
+                for (int i = 0; i < max_count; i++)
+                {
+                    foreach (List<string> ls in writableContent)
+                    {
+                        if(i<ls.Count)
+                            content += ls[i];
+                    }
+                    file.WriteLine(content);
+                    content = "";
+                }
+            }
+        }
+
+        private void readFile(string path,string separator,string name) {
+            string line;
+            bool foundHeaders=false;
+            int counter = 0;
+            List<string> analysisData = new List<string>();
+            List<int> indices = new List<int>();
+            System.IO.StreamReader file = new System.IO.StreamReader(path);
+            while ((line = file.ReadLine()) != null)
+            {
+                List<string> structureAnalysis = line.Split(separator.ToCharArray()).ToList();
+                if (line.Contains(lstofColoumns.Items[0].ToString()) && foundHeaders == false)
+                {
+                    foundHeaders = true;
+                    //foreach (var obj in lstofColoumns.Items) {
+                    for (int i = 0; i < lstofColoumns.Items.Count; i++)
+                    {
+                        var obj = lstofColoumns.Items[i];
+                        if (!line.Contains(obj.ToString()))
+                        {
+                            foundHeaders = false;
+                            break;
+                        }
+                        else
+                        {
+                            indices.Add(structureAnalysis.IndexOf(obj.ToString()));
+                        }
+                    }
+                    if (foundHeaders == true) {
+                        //adds name with titles
+                        string titleLine =","+name;
+                        for (int i = 0; i < lstofColoumns.Items.Count; i++) {
+                            titleLine += ",";
+                        }
+                        analysisData.Add(titleLine);
+                        titleLine = ",";
+                        foreach (int q in indices)
+                        {
+                            titleLine += structureAnalysis[q] + ",";
+                        }
+                        analysisData.Add(titleLine);
+                    }
+                }
+                else if (foundHeaders == true) {
+                    string inputline = ",";
+                    foreach(int q in indices) {
+                        inputline+=structureAnalysis[q]+",";
+                    }
+                    analysisData.Add(inputline);
+                }
+                counter++;
+            }
+            writableContent.Add(analysisData);
+            file.Close();
+        }
+
+        private void rmFileRef_Click(object sender, EventArgs e)
+        {
+            LstOfNames.Items.RemoveAt(FileFnd.SelectedIndex);
+            FileFnd.Items.RemoveAt(FileFnd.SelectedIndex);
+        }
+    }
+
+}
